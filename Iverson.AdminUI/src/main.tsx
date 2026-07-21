6bfcabaa559f6791b0ab8fda6d718607657bb6eb
counsellorben <counsellorben@gmail.com>
2026-07-21 18:34:05 -0400

    feat(admin-ui): wire MUI theme and brand font into the app root

--- Iverson.AdminUI/src/main.tsx
+++ Iverson.AdminUI/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import "@fontsource/fraunces/900.css";
+import { theme } from "./theme/theme";
 import { router } from "./router";
 import { AuthProvider } from "./auth/AuthProvider";
 import { initTelemetry } from "./telemetry";
@@ -9,8 +12,11 @@ initTelemetry();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} future={{ v7_startTransition: true }} />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
