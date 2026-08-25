9e382a2e1c55d4cfd0318ed0ac605bcefa509b7d
counsellorben <counsellorben@gmail.com>
2026-08-24 21:51:41 -0400

    terraform fmt: align storage class keys after adding prometheus

--- Iverson.Server/deploy/terraform/modules/operators/outputs.tf
+++ Iverson.Server/deploy/terraform/modules/operators/outputs.tf
@@ -1,10 +1,10 @@
 output "storage_class_names" {
   value = {
-    postgres  = kubernetes_storage_class.postgres.metadata[0].name
-    starrocks = kubernetes_storage_class.starrocks.metadata[0].name
-    qdrant    = kubernetes_storage_class.qdrant.metadata[0].name
-    kafka     = kubernetes_storage_class.kafka.metadata[0].name
-    ollama    = kubernetes_storage_class.ollama.metadata[0].name
+    postgres   = kubernetes_storage_class.postgres.metadata[0].name
+    starrocks  = kubernetes_storage_class.starrocks.metadata[0].name
+    qdrant     = kubernetes_storage_class.qdrant.metadata[0].name
+    kafka      = kubernetes_storage_class.kafka.metadata[0].name
+    ollama     = kubernetes_storage_class.ollama.metadata[0].name
     prometheus = kubernetes_storage_class.prometheus.metadata[0].name
   }
 }
